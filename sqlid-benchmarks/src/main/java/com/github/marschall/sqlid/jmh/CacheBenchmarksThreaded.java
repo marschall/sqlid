5f7345c9a2f2d8ce18543e276d008d3726c34c6c
Philippe Marschall <philippe.marschall@gmail.com>
2021-06-12 19:30:36 +0200

    update benchmarks

--- sqlid-benchmarks/src/main/java/com/github/marschall/sqlid/jmh/CacheBenchmarksThreaded.java
+++ sqlid-benchmarks/src/main/java/com/github/marschall/sqlid/jmh/CacheBenchmarksThreaded.java
@@ -3,43 +3,53 @@ package com.github.marschall.sqlid.jmh;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.openjdk.jmh.annotations.Mode.Throughput;
 
-import java.util.function.Function;
-
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.infra.Blackhole;
 import org.springframework.util.ConcurrentLruCache;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.marschall.sqlid.Cache;
 import com.github.marschall.sqlid.HashLruCache;
+import com.github.marschall.sqlid.SqlId;
 
 /**
- * Micro-benchmarks for LRU caches with a minimal load function.
+ * Multi-threaded micro-benchmarks for LRU caches with a realistic load function.
  */
 @BenchmarkMode(Throughput)
 @OutputTimeUnit(MILLISECONDS)
 @State(Scope.Benchmark)
-public class CacheBenchmarks {
+@Threads(8)
+public class CacheBenchmarksThreaded {
 
   private static final int CAPACITY = 256;
+  
+  private static final String[] QUERIES;
+  
+  static {
+    QUERIES = new String[CAPACITY + 1];
+    for (int i = 0; i < QUERIES.length; i++) {
+      QUERIES[i] = "SELECT * from dual where dummy = " + i;
+    }
+  }
 
   /*
    * https://github.com/spring-projects/spring-framework/issues/26320
    */
-  private ConcurrentLruCache<Integer, Integer> springCache;
+  private ConcurrentLruCache<String, String> springCache;
 
-  private Cache<Integer, Integer> projectCache;
+  private Cache<String, String> projectCache;
 
-  private com.github.benmanes.caffeine.cache.Cache<Integer, Integer> caffeineCache;
+  private com.github.benmanes.caffeine.cache.Cache<String, String> caffeineCache;
 
   @Setup
   public void doSetup() {
-    this.springCache = new ConcurrentLruCache<>(CAPACITY, Function.identity());
+    this.springCache = new ConcurrentLruCache<>(CAPACITY, SqlId::compute);
     this.projectCache = new HashLruCache<>(CAPACITY);
     this.caffeineCache = Caffeine.newBuilder()
             .initialCapacity(CAPACITY)
@@ -48,10 +58,10 @@ public class CacheBenchmarks {
 
     // preload the caches
     for (int i = 0; i < CAPACITY; i++) {
-      Integer key = i;
-      this.springCache.get(key);
-      this.projectCache.get(key, Function.identity());
-      this.caffeineCache.put(key, key);
+      String query = QUERIES[i];
+      this.springCache.get(query);
+      this.projectCache.get(query, SqlId::compute);
+      this.caffeineCache.put(query, SqlId.compute(query));
     }
   }
 
@@ -59,7 +69,8 @@ public class CacheBenchmarks {
   public void getInCapacitySpring(Blackhole blackhole) {
     for (int i = 0; i < CAPACITY; i++) {
       for (int j = 0; j < i; j++) {
-        blackhole.consume(this.springCache.get(j));
+        String query = QUERIES[j];
+        blackhole.consume(this.springCache.get(query));
       }
     }
   }
@@ -68,7 +79,8 @@ public class CacheBenchmarks {
   public void getInCapacityCaffeine(Blackhole blackhole) {
     for (int i = 0; i < CAPACITY; i++) {
       for (int j = 0; j < i; j++) {
-        blackhole.consume(this.caffeineCache.get(j, Function.identity()));
+        String query = QUERIES[j];
+        blackhole.consume(this.caffeineCache.get(query, SqlId::compute));
       }
     }
   }
@@ -77,7 +89,8 @@ public class CacheBenchmarks {
   public void getInCapacityProject(Blackhole blackhole) {
     for (int i = 0; i < CAPACITY; i++) {
       for (int j = 0; j < i; j++) {
-        blackhole.consume(this.projectCache.get(j, Function.identity()));
+        String query = QUERIES[j];
+        blackhole.consume(this.projectCache.get(query, SqlId::compute));
       }
     }
   }
@@ -85,21 +98,24 @@ public class CacheBenchmarks {
   @Benchmark
   public void getOutOfCapacitySpring(Blackhole blackhole) {
     for (int i = 0; i < (CAPACITY + 1); i++) {
-      blackhole.consume(this.springCache.get(i));
+      String query = QUERIES[i];
+      blackhole.consume(this.springCache.get(query));
     }
   }
 
   @Benchmark
   public void gettOutOfCapacityCaffeine(Blackhole blackhole) {
     for (int i = 0; i < (CAPACITY + 1); i++) {
-      blackhole.consume(this.caffeineCache.get(i, Function.identity()));
+      String query = QUERIES[i];
+      blackhole.consume(this.caffeineCache.get(query, SqlId::compute));
     }
   }
 
   @Benchmark
   public void gettOutOfCapacityProject(Blackhole blackhole) {
     for (int i = 0; i < (CAPACITY + 1); i++) {
-      blackhole.consume(this.projectCache.get(i, Function.identity()));
+      String query = QUERIES[i];
+      blackhole.consume(this.projectCache.get(query, SqlId::compute));
     }
   }
 
