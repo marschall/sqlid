615df7b37ca315b0aef1689e15775f535f1cf72e
Philippe Marschall <philippe.marschall@gmail.com>
2021-07-04 22:20:35 +0200

    introduce model

---
+++ sqlid-gui/src/main/java/com/github/marschall/sqlid/gui/SqlGuiModel.java
@@ -0,0 +1,109 @@
+package com.github.marschall.sqlid.gui;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.github.marschall.sqlid.SqlId;
+
+import oracle.jdbc.pool.OracleDataSource;
+
+final class SqlGuiModel {
+  
+  private volatile String url;
+  
+  private volatile String user;
+  
+  private volatile String password;
+  
+  private volatile String query;
+  
+  private volatile boolean nativeSql;
+
+  SqlGuiModel() {
+    super();
+  }
+
+  String getUrl() {
+    return url;
+  }
+
+  void setUrl(String url) {
+    this.url = url;
+  }
+
+  String getUser() {
+    return user;
+  }
+
+  void setUser(String user) {
+    this.user = user;
+  }
+
+  String getPassword() {
+    return password;
+  }
+
+  void setPassword(String password) {
+    this.password = password;
+  }
+
+  String getQuery() {
+    return query;
+  }
+
+  void setQuery(String query) {
+    this.query = query;
+  }
+
+  boolean isNativeSql() {
+    return nativeSql;
+  }
+
+  void setNativeSql(boolean nativeSql) {
+    this.nativeSql = nativeSql;
+  }
+  
+  String computeSqlId() throws SQLException {
+    String oracleSql;
+    if (this.nativeSql) {
+      oracleSql = this.query;
+    } else {
+      oracleSql = computeNativeSql();
+    }
+    return SqlId.compute(oracleSql);
+  }
+  
+  private String computeNativeSql() throws SQLException {
+    DataSource dataSource = createDataSource();
+    try (Connection connection = dataSource.getConnection()) {
+      return connection.nativeSQL(this.query);
+    }
+  }
+  
+  private DataSource createDataSource() throws SQLException {
+    OracleDataSource dataSource = new OracleDataSource();
+    dataSource.setURL(this.url);
+    dataSource.setUser(this.user);
+    dataSource.setPassword(this.password);
+    return dataSource;
+  }
+  
+  boolean isValid() {
+    if (this.nativeSql) {
+      return !isEmpty(this.query);
+    } else {
+      return !isEmpty(this.url)
+          && !isEmpty(this.user)
+          && !isEmpty(this.password)
+          && !isEmpty(this.query);
+    }
+    
+  }
+  
+  private static boolean isEmpty(String s) {
+    return s == null || s.isEmpty();
+  }
+
+}
--- sqlid-gui/src/main/java/com/github/marschall/sqlid/gui/SqlIdGui.java
+++ sqlid-gui/src/main/java/com/github/marschall/sqlid/gui/SqlIdGui.java
@@ -2,13 +2,21 @@ package com.github.marschall.sqlid.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.sql.SQLException;
 
-import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -20,10 +28,30 @@ public final class SqlIdGui {
     } catch (ReflectiveOperationException | UnsupportedLookAndFeelException e) {
       System.err.println("System LaF not supported");
     }
-    SwingUtilities.invokeLater(SqlIdGui::createAndShowGui);
+    SqlIdGui gui = new SqlIdGui();
+    SwingUtilities.invokeLater(gui::createAndShowGui);
   }
 
-  private static void createAndShowGui() {
+  private final SqlGuiModel model;
+  private JButton computeButton;
+
+  SqlIdGui() {
+    this.model = new SqlGuiModel();
+  }
+
+  void computeSqlId() {
+    // TODO disable GUI
+    // TODO enable GUI
+    ComputeSqlId task = new ComputeSqlId();
+    task.execute();
+  }
+  
+  void showException(SQLException exception) {
+    // TODO frame
+    JOptionPane.showMessageDialog(null, exception.getMessage(), "SQL Exception", JOptionPane.ERROR_MESSAGE);
+  }
+
+  private void createAndShowGui() {
     JFrame frame = new JFrame("SQL_ID GUI");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -31,17 +59,79 @@ public final class SqlIdGui {
 
     addTextArea(contentPane);
 
-    contentPane.add(new JButton("Compute SQL_ID"), BorderLayout.PAGE_END);
+    contentPane.add(createBottomPanel(), BorderLayout.PAGE_END);
 
     frame.pack();
+    frame.setMinimumSize(frame.getSize());
 
     frame.setVisible(true);
   }
 
-  private static void addTextArea(Container container) {
+  private void addTextArea(Container container) {
     JTextArea textArea = new JTextArea(40, 80);
     JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     container.add(scrollPane, BorderLayout.CENTER);
+  }
+
+  private JPanel createBottomPanel() {
+    JPanel bottomPanel = new JPanel(new BorderLayout());
+    bottomPanel.add(createConnectionPanel(), BorderLayout.LINE_START);
+    bottomPanel.add(createButtonPanel(), BorderLayout.LINE_END);
+    return bottomPanel;
+  }
+
+  private JPanel createButtonPanel() {
+    JPanel buttonPanel = new JPanel();
+    computeButton = new JButton("Compute SQL_ID");
+    computeButton.addActionListener(e -> computeSqlId());
+    buttonPanel.add(computeButton);
+    return buttonPanel;
+  }
+
+  private JPanel createConnectionPanel() {
+    JPanel connectionPanel = new JPanel(new GridBagLayout());
+    GridBagConstraints constraints;
+
+    String[] labels = {"URL:", "User:", "Password:"};
+    for (int i = 0; i < labels.length; i++) {
+      connectionPanel.add(new JLabel(labels[i]), connectionLabelConstraints(i));
+    }
+
+    constraints = connectionFieldConstraints(0);
+    connectionPanel.add(new JTextField(32), constraints);
+
+    constraints = connectionFieldConstraints(1);
+    connectionPanel.add(new JTextField(32), constraints);
+
+    constraints = connectionFieldConstraints(2);
+    connectionPanel.add(new JPasswordField(32), constraints);
+
+    return connectionPanel;
+  }
+
+  private GridBagConstraints connectionLabelConstraints(int i) {
+    GridBagConstraints constraints = new GridBagConstraints();
+    constraints.gridx = 0;
+    constraints.gridy = i;
+    constraints.anchor = GridBagConstraints.LINE_END;
+    return constraints;
+  }
+
+  private GridBagConstraints connectionFieldConstraints(int i) {
+    GridBagConstraints constraints = new GridBagConstraints();
+    constraints.gridx = 1;
+    constraints.gridy = i;
+    constraints.anchor = GridBagConstraints.LINE_START;
+    return constraints;
+  }
+
+  final class ComputeSqlId extends SwingWorker<String, String> {
+
+    @Override
+    protected String doInBackground() throws Exception {
+      return model.computeSqlId();
+    }
+
   }
 
 }
